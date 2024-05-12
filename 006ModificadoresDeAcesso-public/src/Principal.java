96ecce7fad6901d3f96b836b330b7fbb86f363be
icaro josep <147887309+IcaroJosep@users.noreply.github.com>
2024-05-12 13:29:02 -0300

    .

--- 006ModificadoresDeAcesso-public/src/Principal.java
+++ 006ModificadoresDeAcesso-public/src/Principal.java
@@ -1,6 +1,18 @@
 
 import discentes.AlunoSuperior;
 
+/*		public:
+ * 	
+ * 	permite q possao ser acessadas de qualquer lugar
+ *  clase metodos ou pacotes diferentes
+ * 
+ *	 aqui neste codigo ha a demostracao de como mesmo em pacotes e clases diferentes 
+ *   ha o acesso publico das variaveis
+ * 
+ * */
+
+
+
 public class Principal {
 
 	public static void main(String[] args) {
