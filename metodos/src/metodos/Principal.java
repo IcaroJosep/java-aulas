d1a8c5e9ca3fd60151de5c38127ec07c74344305
icaro josep <147887309+IcaroJosep@users.noreply.github.com>
2024-06-30 13:01:57 -0300

    atualuzacao

---
+++ metodos/src/metodos/Principal.java
@@ -0,0 +1,17 @@
+package metodos;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+	
+	public static void canal() {
+		System.out.println("qwe");
+	}
+	
+	
+	
+}
